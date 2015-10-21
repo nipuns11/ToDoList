82a44f103d8565ddb19768e11284b7daec71b0b2
nipuns11 <nipunsharma@nipuns-Mac-mini.local>
2015-10-21 17:51:18 -0400

    a  basic to do list app
    a basic to do list app in swift . lets user add list items and delete
    them

---
+++ ToDoList/FirstViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.swift
+//  ToDoList
+//
+//  Created by nipun sharma on 2015-10-11.
+//  Copyright © 2015 nipun. All rights reserved.
+//
+
+import UIKit
+
+var toDoList = [String]()
+
+class FirstViewController: UIViewController , UITableViewDelegate {
+
+    @IBOutlet weak var toDoListTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            
+        toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+       
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        cell.backgroundColor = self.view.backgroundColor
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            toDoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            
+            toDoListTable.reloadData()
+        
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+
+}
+
---
+++ ToDoList/SecondViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by nipun sharma on 2015-10-11.
+//  Copyright © 2015 nipun. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var item: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text!)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+       item.resignFirstResponder()
+        return true
+    }
+
+}
+
